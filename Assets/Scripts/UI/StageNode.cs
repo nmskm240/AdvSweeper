0d6956fc180453ec2d5582e64e5671011b260655
nmskm240 <bokaronogod@gmail.com>
2021-04-29 20:57:01 +0900

    ステージ選択の最適化

--- Assets/Scripts/UI/StageNode.cs
+++ Assets/Scripts/UI/StageNode.cs
@@ -2,28 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using MultiSceneManagement;
 using Adv;
 
 namespace UI
 {    
-    public class StageNode : MonoBehaviour
+    public class StageNode : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField]
         private Text _text;
         [SerializeField]
-        private StageData _stageData;
+        private StageData _base;
+        [SerializeField]
+        private StageData _loadStageData;
 
-        public string Name{ get { return _text.text; } set { _text.text = value; } }
-
-        public void Load()
+        private void Awake() 
         {
-            var loadData = Instantiate(Resources.Load("Datas/Stage/" + Name) as StageData);
-            _stageData.SetName(loadData.Name);
-            _stageData.SetFloor(loadData.Floor);
-            _stageData.SetSpawnTable(new List<EnemyData>(loadData.SpawnTable));
-            _stageData.SetItemTable(new List<ItemData>(loadData.ItemTable));
-            SceneManager.LoadScene("Game");
+            _text.text = _base.Name;
+        }
+
+        public void OnPointerClick(PointerEventData e)
+        {
+            _loadStageData.SetName(_base.Name);
+            _loadStageData.SetFloor(_base.Floor);
+            _loadStageData.SetSpawnTable(new List<EnemyData>(_base.SpawnTable));
+            _loadStageData.SetItemTable(new List<ItemData>(_base.ItemTable));
+            MultiSceneManager.LoadScene("Game");
         }
     }
 }
