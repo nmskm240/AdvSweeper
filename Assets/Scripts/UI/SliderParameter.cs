fa16722d11a5cc906f3d9168b31c98896518dfd2
nmskm240 <bokaronogod@gmail.com>
2021-05-24 15:39:32 +0900

    SliderParameterの汎用化

--- Assets/Scripts/UI/SliderParameter.cs
+++ Assets/Scripts/UI/SliderParameter.cs
@@ -8,14 +8,34 @@ namespace UI
     public class SliderParameter : MonoBehaviour
     {
         [SerializeField]
+        private SliderParameterOptions _options = SliderParameterOptions.ValueNone;
+        [SerializeField]
         private TextMeshProUGUI _text;
+        [SerializeField]
+        private TextMeshProUGUI _value;
         [SerializeField]
         private Slider _slider;
 
         public string Name{ get { return _text.text; } }
         public float Min{ get { return _slider.minValue; } }
         public float Max{ get { return _slider.maxValue; } }
-        public float Value{ get { return _slider.value; } set{ _slider.DOValue(value, 1f); } }
+        public float Value
+        { 
+            get { return _slider.value; } 
+            set
+            { 
+                _slider.DOValue(value, 1f).OnComplete(() => 
+                {
+                    _value.text = (_options == SliderParameterOptions.ValueOnly) ? 
+                        _slider.value.ToString() : _slider.value + "/" + Max;
+                }); 
+            } 
+        }
+
+        private void Awake()
+        {
+            _value.enabled = (_options != SliderParameterOptions.ValueNone);
+        }
 
         public void Init(string name, int min, int max)
         {
---
+++ Assets/Scripts/UI/SliderParameterOptions.cs
@@ -0,0 +1,9 @@
+namespace UI
+{
+    public enum SliderParameterOptions
+    {
+        ValueNone,
+        ValueOnly,
+        ValueAndMax,
+    }
+}
