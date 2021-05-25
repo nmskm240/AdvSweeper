7b215a07a8dc1f684b962d723b4dd7a6cf855e53
nmskm240 <bokaronogod@gmail.com>
2021-05-25 13:32:26 +0900

    アイテムに特性を持たせるための準備

--- Assets/Scripts/Alchemy/CharacteristicsData.cs
+++ Assets/Scripts/Alchemy/CharacteristicsData.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using Adv.Effects;
 
 namespace Alchemy 
 {     
     [CreateAssetMenu(fileName = "CharacteristicsData", menuName = "AdvSweeper/CharacteristicsData", order = 0)]
     public class CharacteristicsData : BaseData
     {
-
+        [SerializeReference, SubclassSelector]
+        private IEffect _effect;
     }
 }
